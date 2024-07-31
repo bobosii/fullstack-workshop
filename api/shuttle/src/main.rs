d6a7bf711a4e2e7bc7f97dbbd835a1f36b0fae96
bobosii <day.emir@hotmail.com>
2024-07-31 17:39:15 +0300

    add models

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -13,8 +13,9 @@ async fn actix_web(
         .map_err(CustomError::new)?;
     let pool = actix_web::web::Data::new(pool);
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(pool).configure(health::service)
-        .configure(api_lib::films::service);
+        cfg.app_data(pool)
+            .configure(health::service)
+            .configure(api_lib::films::service);
     };
     Ok(config.into())
 }
